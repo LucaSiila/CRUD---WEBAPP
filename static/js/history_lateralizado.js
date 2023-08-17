c0daee84a19d534c2005417aa5c5b20ac167e3a2
LucaCGN <caco.nonino@outlook.com.br>
2023-08-17 16:31:25 -0300

    test commit

--- static/js/history_lateralizado.js
+++ static/js/history_lateralizado.js
@@ -6,7 +6,7 @@
 
 	// Initialize the Tabulator table
 	$(document).ready(function () {
-		console.log("Initializing Tabulator..."); #test commit
+		console.log("Initializing Tabulator..."); 
 		var table = new Tabulator("#history-lateralizado-table", {
 			layout: "fitData", // Fit data and allow stretching
 			frozenColumns: 5, // Freeze the first 5 columns
